d9b63f63869a598916dcd2a0a92b23d07007dfc9
Moronicfoil <ibarrapaz0405@gmail.com>
2025-05-13 10:12:05 -0600

    matriz arreglada

--- P1/MatrizAdyacente.c
+++ P1/MatrizAdyacente.c
@@ -7,7 +7,7 @@ int main(){
     char abc = 97;
 
     int colocador = 0;
-    int mov =1;
+    int mov = 1;
     int delimitador = 8;
 
     int true = 1;
@@ -15,17 +15,17 @@ int main(){
     //inicializar la matriz
     for (int i = 0; i < 8; i++)
     {
-        for (int j = 0; j < j; j++)
+        for (int j = 0; j < 8; j++)
         {
             matriz[i][j] = 0;
         }
         
     }
-
+    
     //toma de valores del usuario
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 7; i++)
     {
-        for (int j = 0; j < delimitador; j++)
+        for (int j = 0; j <= 7; j++)
         {
             //nos saltamos los valores que ya colocamos 
             if(true ==1){
@@ -37,8 +37,8 @@ int main(){
             //recoleccion de datos
             printf("Escriba los valores ponderados para %c, matriz de adyacencia [%c][%c]: \n", nodos, nodos, abc);
             scanf("%d", &colocador);
-            matriz[i][j+ mov] = colocador;
-            matriz[j+mov][i] = colocador;
+            matriz[i][j] = colocador;
+            matriz[j][i] = colocador;
             abc++;
     
         }
@@ -48,6 +48,19 @@ int main(){
         abc = 97;
         mov++;
     }
+    printf("\n");
+
+    //imprime la matriz resultante 
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            printf("%d \t", matriz[i][j]);
+        }
+        printf("\n");
+        
+    }
+    
 
 }
         
